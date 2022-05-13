0c0bc4f592fe3e124aa970947f29462c33ecbfb0
RonaldasC <ronaldasr@gmail.com>
2022-05-13 11:22:15 +0300

    player && ai veikia

--- script.js
+++ script.js
@@ -1,20 +1,49 @@
 'use strict';
+//kodo dalis atsakinga už ai stulpelio didėjimą
+let aiSkaiciuokle = 0;
 
-let skaiciuokle = 0;
-let laikoskaiciuoklė = setInterval(naujasKintamasis, 100);
+//function laikoSkaiciuokleSustabdyta();
+//setTimeout(laikoskaiciuokle, 90000);
+
+let laikoskaiciuokle = setInterval(naujasKintamasis, 100);
+
+// let stopedLaikoSkaiciuokle = clearInterval(laikoskaiciuokle);
+
+
+// document.getElementsByClassName('langas')[0].addEventListener('click', start());
+
+// function start() {
+//     let laikoskaiciuokle = setInterval(naujasKintamasis, 100);
+// }
 
 function naujasKintamasis() {
-    skaiciuokle++;
-    if (skaiciuokle === 300){
-        clearInterval(laikoskaiciuoklė);
+    aiSkaiciuokle++;
+    if (aiSkaiciuokle === 300){
+        clearInterval(laikoskaiciuokle);
     }
-    aukscioAtvaizdavimas();
-    console.log(laikoskaiciuoklė);
+    aiAukscioAtvaizdavimas();
 }
-function aukscioAtvaizdavimas() {
-    document.getElementById('ai').style.height = skaiciuokle + 'px';
-    document.getElementById('stulpelis').style.height = skaiciuokle+'px';
+function aiAukscioAtvaizdavimas() {
+    document.getElementById('ai').style.height = aiSkaiciuokle + 'px';
 }
+//kodo dalis atsakinga už player stulpelio didėjimą
+let playerSkaiciuokle = 0;
 
+document.getElementsByClassName('langas')[0].addEventListener('click', playerMouseClicks);
+
+function playerMouseClicks() {
+
+    playerSkaiciuokle++;
+    if (playerSkaiciuokle === 300){
+        console.log('Laimėjai');//pakeisti į laimėjimo langą
+        clearInterval(laikoskaiciuokle);//sutabdo ai stulpelio didėjima jei laimi žaidėjas
+        return playerSkaiciuokle = 300;
+    } 
+    playerAukscioAtvaizdavimas();
+    
+}
+function playerAukscioAtvaizdavimas() {
+    document.getElementById('player').style.height = playerSkaiciuokle + 'px';
+}
 
 
